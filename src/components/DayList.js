4577c3ea35139cc3d918fcae3118ba96790ae974
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-04-18 19:19:22 -0400

    DayList with iteration

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import DayListItem from './DayListItem';
 
-export default function DayList(){
+export default function DayList(props){
 
-  return (
-
+  const dayArray = props.days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  
+        />
+        )
+      })
+      
+      
+      return (
+        
+        
     <ul>
 
+    
+      {dayArray}
+
+      
     </ul>
 
   );
