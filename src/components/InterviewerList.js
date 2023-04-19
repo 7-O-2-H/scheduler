abc814761ec007639998caca48d034489d3cac83
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-04-19 17:20:07 -0400

    Int List

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
 
-export default function InterviewList(props) {
+export default function InterviewerList(props) {
+
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const intArray = interviewers.map(int => {
+    return (
+      <InterviewerListItem
+        key={int.id}
+        name={int.name}
+        avatar={int.avatar}
+        selected={int.id === interviewer}
+        setInterviewer={setInterviewer}
+      />
+    )
+  })
 
   return (
 
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {intArray}
+      </ul>
     </section>
 
   )
--- stories/index.js
+++ stories/index.js
@@ -9,6 +9,7 @@ import DayListItem from "components/DayListItem";
 import Button from "components/Button";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 storiesOf("Button", module)
   .addParameters({
