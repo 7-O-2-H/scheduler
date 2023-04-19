dca50151e2d0351d2fb0000ba432d7ced995bd8a
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-04-18 20:27:33 -0400

    add useState hook to app.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DayList from "./DayList";
-import Button from "./Button";
 import "components/Application.scss";
 
 const days = [
@@ -23,6 +22,9 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -35,8 +37,8 @@ export default function Application(props) {
       <nav className="sidebar__menu">
         <DayList
           days={days}
-          day={"Monday"}
-          setDay={day => console.log(day)}
+          day={day}
+          setDay={setDay}
         />  
       </nav>
       <img
